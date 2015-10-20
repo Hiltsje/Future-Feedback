a43492415241a75d2cdcc7c9a85a07f86f2bed69
hiltsje <hillie@sengo.nl>
2015-10-20 16:00:17 +0200

    Add CRUD functionality and views for decisions

--- app/controllers/decisions_controller.rb
+++ app/controllers/decisions_controller.rb
@@ -1,4 +1,52 @@
 class DecisionsController < ApplicationController
+before_action :find_decision, only: [:show, :edit, :update, :destroy]
+
   def index
+# gets all rows from decision table and puts it in @decision variable
+    @decisions = Decision.all
   end
+
+  def show
+# find only the decision entry that has the id defined in params[:id]
+  end
+# shows the form for creating a entry
+  def new
+    @decision = Decision.new
+  end
+# creates the entry
+  def create
+    @decision = Decision.new(decision_params)
+      if @decision.save
+        redirect_to @decision
+      else
+        render 'new'
+      end
+  end
+# shows the form for editing a entry
+  def edit
+  end
+# updates the entry
+  def update
+    if @decision.update(decision_params)
+      redirect_to @decision
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @decision.destroy
+    redirect_to root_path
+  end
+
+  private
+  def find_decision
+# Loads the right entry.
+      @decision = Decision.find(params["id"])
+  end
+
+  def decision_params
+    params.require(:decision).permit(:title, :forecast, :review_date)
+  end
+
 end
